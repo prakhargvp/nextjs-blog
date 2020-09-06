f6dbb412d6919a5f7170ec4f3baf9b18e9f583c3
Prakhar Agarwal <prakhargvp@gmail.com>
2020-09-07 05:05:45 +0530

    Added Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Layout, { siteTitle } from '../components/layout'
+import Date from '../components/date'
+
 
 export default function Home({ allPostsData }) {
   return (
@@ -21,7 +24,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title} <br /> {id} <br /> {date}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a> 
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))} 
         </ul>
